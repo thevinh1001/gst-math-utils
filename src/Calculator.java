7d96ab98f48174896ec19dd59ef5ebb26f141210
Nguyen The Vinh <nguyenthevinhpesss@gmail.com>
2025-09-26 13:28:14 +0700

    feat: add Calculator class with basic math functions

---
+++ src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    public int plus(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+    if (b == 0) throw new ArithmeticException("Cannot divide by zero");
+    return (double) a / b;
+}
+
+}
